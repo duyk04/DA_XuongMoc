1b12cb442703c8de0d096b0861b5e1f761f4b36b
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2024-02-22 22:14:47 +0700

    22/02

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -37,58 +37,190 @@ function Home() {
                 </div>
             </div>
             <div className='Categories w-100 d-flex justify-content-center'>
-                <div className='list-categories box-container'>
-                    <div className='row'>
-                        <div className='col-md-3'>
-                            <a href='#'>
-                                <img src='../images/AnhCatTC/loai-sp/icon-phong-khach.png' />
-                                <p>Phòng khách</p>
-                            </a>
+                <div className='box-container'>
+                    <div className='list-categories w-100'>
+                        <div className='row'>
+                            <div className='col-md-3'>
+                                <a href='#'>
+                                    <img src='../images/AnhCatTC/loai-sp/icon-phong-khach.png' alt='img' />
+                                    <p>Phòng khách</p>
+                                </a>
+                            </div>
+                            <div className='col-md-3'>
+                                <a href='#'>
+                                    <img src='../images/AnhCatTC/loai-sp/icon-phong-ngu.png' alt='img' />
+                                    <p>Phòng ngủ</p>
+                                </a>
+                            </div>
+                            <div className='col-md-3'>
+                                <a href='#'>
+                                    <img src='../images/AnhCatTC/loai-sp/icon-phong-bep.png' alt='img' />
+                                    <p>Phòng bếp</p>
+                                </a>
+                            </div>
+                            <div className='col-md-3'>
+                                <a href='#'>
+                                    <img src='../images/AnhCatTC/loai-sp/icon-phong-tam.png' alt='img' />
+                                    <p>Phòng tắm</p>
+                                </a>
+                            </div>
+                            <div className='col-md-3'>
+                                <a href='#'>
+                                    <img src='../images/AnhCatTC/loai-sp/icon-tre-em.png' alt='img' />
+                                    <p>Trẻ em</p>
+                                </a>
+                            </div>
+                            <div className='col-md-3'>
+                                <a href='#'>
+                                    <img src='../images/AnhCatTC/loai-sp/icon-van-phong.png' alt='img' />
+                                    <p>Văn Phòng</p>
+                                </a>
+                            </div>
+                            <div className='col-md-3'>
+                                <a href='#'>
+                                    <img src='../images/AnhCatTC/loai-sp/icon-cau-thang.png' alt='img' />
+                                    <p>Cầu thang</p>
+                                </a>
+                            </div>
+                            <div className='col-md-3'>
+                                <a href='#'>
+                                    <img src='../images/AnhCatTC/loai-sp/icon-do-trang-tri.png' alt='img' />
+                                    <p>Đồ trang trí</p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='slide-product mt-5'>
+                        <h4>SẢN PHẨM NỔI BẬT</h4>
+                    </div>
+                    <div className='list-product d-flex align-items-center'>
+                        <div>
+                            <button>
+                                <img src='..\images\AnhCatTC\button-left.png' />
+                            </button>
                         </div>
                         <div className='col-md-3'>
                             <a href='#'>
-                                <img src='../images/AnhCatTC/loai-sp/icon-phong-ngu.png' />
-                                <p>Phòng ngủ</p>
+                                <div style={{ overflow: 'hidden'}}>
+                                    <img src='../images/AnhCatTC/san-pham-noi-bat/sp-1.jpg' alt='img' className='img-hover' />
+                                </div>
+                                <p className='mb-1'>Giường châu âu</p>
                             </a>
+                            <div>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg mx-2" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg mx-2" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg" style={{ color: "#FFD43B" }} />
+                                </span>
+                            </div>
+                            <p className='describe'>
+                                (Size lớn, trắng sữa)
+                            </p>
+                            <p className='mt-0'>8.999.999 VND</p>
                         </div>
                         <div className='col-md-3'>
                             <a href='#'>
-                                <img src='../images/AnhCatTC/loai-sp/icon-phong-bep.png' />
-                                <p>Phòng bếp</p>
+                                <img src='../images/AnhCatTC/san-pham-noi-bat/sp-2.jpg' alt='img' />
+                                <p className='mb-1'>Bàn làm việc</p>
                             </a>
+                            <div>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg mx-2" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg mx-2" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg" style={{ color: "#FFD43B" }} />
+                                </span>
+                            </div>
+                            <p className='describe'>
+                                (Size lớn, trắng nâu)
+                            </p>
+                            <p className='mt-0'>3.999.999 VND</p>
                         </div>
                         <div className='col-md-3'>
                             <a href='#'>
-                                <img src='../images/AnhCatTC/loai-sp/icon-phong-tam.png' />
-                                <p>Phòng tắm</p>
+                                <img src='../images/AnhCatTC/san-pham-noi-bat/sp-3.jpg' alt='img' />
+                                <p className='mb-1'>Tủ quần áo</p>
                             </a>
+                            <div>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg mx-2" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg mx-2" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg" style={{ color: "#FFD43B" }} />
+                                </span>
+                            </div>
+                            <p className='describe'>
+                                (4 ngăn, trắng gỗ)
+                            </p>
+                            <p className='mt-0'>12.999.999 VND</p>
                         </div>
                         <div className='col-md-3'>
                             <a href='#'>
-                                <img src='../images/AnhCatTC/loai-sp/icon-tre-em.png' />
-                                <p>Trẻ em</p>
+                                <img src='../images/AnhCatTC/san-pham-noi-bat/sp-4.jpg' alt='img' />
+                                <p className='mb-1'>Kệ để đồ</p>
                             </a>
+                            <div>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg mx-2" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg mx-2" style={{ color: "#FFD43B" }} />
+                                </span>
+                                <span>
+                                    <i className="fa-solid fa-star fa-lg" style={{ color: "#FFD43B" }} />
+                                </span>
+                            </div>
+                            <p className='describe'>
+                                (Size lớn, trắng sữa)
+                            </p>
+                            <p className='mt-0'>2.499.999 VND</p>
                         </div>
-                        <div className='col-md-3'>
-                            <a href='#'>
-                                <img src='../images/AnhCatTC/loai-sp/icon-van-phong.png' />
-                                <p>Văn Phòng</p>
-                            </a>
-                        </div>
-                        <div className='col-md-3'>
-                            <a href='#'>
-                                <img src='../images/AnhCatTC/loai-sp/icon-cau-thang.png' />
-                                <p>Cầu thang</p>
-                            </a>
-                        </div>
-                        <div className='col-md-3'>
-                            <a href='#'>
-                                <img src='../images/AnhCatTC/loai-sp/icon-do-trang-tri.png' />
-                                <p>Đồ trang trí</p>
-                            </a>
+                        <div>
+                            <button>
+                                <img src='..\images\AnhCatTC\button-right.png' />
+                            </button>
                         </div>
                     </div>
                 </div>
+
+
+            </div>
+            <div className='Product'>
+
             </div>
         </>
 
