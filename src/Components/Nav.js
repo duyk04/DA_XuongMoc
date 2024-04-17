439b49554f4dc353b4ebed6b459bef12db53e0ea
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2024-04-18 00:08:13 +0700

    Create Nav.js

---
+++ src/Components/Nav.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../style.css'
+function Nav() {
+    return (
+        <div>
+            <nav className='navbar w-100  navbar-light bg-light'>
+                <div className='w-100'>
+                    <div className='float-start'>
+                        <img src='../images/AnhCatTC/logo.png' />
+                    </div>
+                    <div className='float-end d-flex align-items-center' style={{ height: 96 }}>
+                        <div className='navbar-item '>
+                            <ul className='d-flex mb-0 h-100'>
+                                <li className="nav-item mx-4">
+                                    <Link to={"/home"}>Trang chủ</Link>
+                                </li>
+                                <li className="nav-item mx-4">
+                                    <Link to={"/introduce"}>Giới thiệu</Link>
+                                </li>
+                                <li className="nav-item mx-4">
+                                    <Link to={"/products"}>Sản phẩm</Link>
+                                </li>
+                                <li className="nav-item mx-4">
+                                    <Link to={"/new"}>Tin tức</Link>
+                                </li>
+                                <li className="nav-item mx-4">
+                                    <Link to={"/contact"}>Liên hệ</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
