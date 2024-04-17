08406b4f4d625cf0dc525bce2506d40812101bf1
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2024-04-18 00:08:18 +0700

    Create NotFound.js

---
+++ src/Components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div>
+            <h2 style={{color: "red"}}>404 NotFound</h2>
+        </div>
+    )
+}
+
+export default NotFound
