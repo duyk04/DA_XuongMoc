2980237f2b9a1c1af3189622f1d1b61174bb8ea0
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2024-04-18 00:07:58 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
+import { BrowserRouter, Route, Navigate, Routes } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Nav from './Components/Nav';
 import Home from './Components/Home';
 import Footer from './Components/Footer';
+import New from './Components/New';
+import NotFound from './Components/NotFound';
+import Introduce from './Components/Introduce';
+import Products from './Components/Products';
+import Contact from './Components/Contact';
 
 function App() {
 	return (
 		<div className="App">
-			<Navbar expand="lg" className="bg-body-tertiary py-0" style={{ height: 110, }}>
-                <Container className="px-0 navbar mx-0 ">
-                    <Navbar.Brand href="#home"><img src='../../images/AnhCatTC/logo.png'></img></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav" className='al float-end flex-grow-0'>
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Trang Chủ</Nav.Link>
-                            <Nav.Link href='#gioiThieu'>Giới Thiệu</Nav.Link>
-                            <Nav.Link href='#sanPham'>Sản Phẩm</Nav.Link>
-                            <Nav.Link href='#tinTuc'>Tin tức</Nav.Link>
-                            <Nav.Link href='#doiTac'>Đối tác</Nav.Link>
-                            <Nav.Link href='#LienHe'>Liên Hệ</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-			<Home/>
+			<BrowserRouter>
+				{/* Nav */}
+				<Nav />
+				<Routes	>
+					<Route path='/home' element={<Home />}></Route>
+					<Route path='/introduce' element={<Introduce />}></Route>
+					<Route path='/products' element={<Products />}></Route>
+					<Route path='/new' element={<New />}></Route>
+					<Route path='/contact' element={<Contact />}></Route>
 
-			{/* footer */}
-            <Footer/>
+					<Route index element={<Navigate to='/home' />} />
+					<Route path='*' element={<NotFound />}></Route>
+				</Routes>
+				{/* footer */}
+				<Footer />
+			</BrowserRouter>
+			{/* <Nav />
+			<Home />
+			<Footer /> */}
 		</div>
 	);
 }
 
 export default App;
+
