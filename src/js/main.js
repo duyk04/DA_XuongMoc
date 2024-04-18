1eac7ab7bf1bb006d67c6aaddb86381ebfaa7eed
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2024-04-18 23:52:58 +0700

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+// let items = document.querySelectorAll('.carousel .carousel-item')
 
-        items.forEach((el) => {
-            console.log("hi");
-            const minPerSlide = 6
-            let next = el.nextElementSibling
-            for (var i = 1; i < minPerSlide; i++) {
-                if (!next) {
-                    // wrap carousel by using first child
-                    next = items[0]
-                }
-                let cloneChild = next.cloneNode(true)
-                el.appendChild(cloneChild.children[0])
-                next = next.nextElementSibling
-            }
-        })
+//         items.forEach((el) => {
+//             console.log("hi");
+//             const minPerSlide = 6
+//             let next = el.nextElementSibling
+//             for (var i = 1; i < minPerSlide; i++) {
+//                 if (!next) {
+//                     // wrap carousel by using first child
+//                     next = items[0]
+//                 }
+//                 let cloneChild = next.cloneNode(true)
+//                 el.appendChild(cloneChild.children[0])
+//                 next = next.nextElementSibling
+//             }
+//         })
