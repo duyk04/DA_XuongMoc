7df75f22c3eb11c4c075ea165a8aa6b8090c3677
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2024-04-21 23:21:19 +0700

    Update NotFound.js

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -8,13 +8,13 @@ function NotFound() {
         // </div>
         <>
             <h1>404 Error Page</h1>
-            <p class="zoom-area"><b>CSS</b> animations to make a cool 404 page. </p>
-            <section class="error-container">
-                <span class="four"><span class="screen-reader-text">4</span></span>
-                <span class="zero"><span class="screen-reader-text">0</span></span>
-                <span class="four"><span class="screen-reader-text">4</span></span>
+            <p className="zoom-area"><b>CSS</b> animations to make a cool 404 page. </p>
+            <section className="error-container">
+                <span className="four"><span className="screen-reader-text">4</span></span>
+                <span className="zero"><span className="screen-reader-text">0</span></span>
+                <span className="four"><span className="screen-reader-text">4</span></span>
             </section>
-            <div class="link-container">
+            <div className="link-container">
                 <Link className='more-link' to={'/home'}>Tới trang chủ</Link>
             </div>
         </>
