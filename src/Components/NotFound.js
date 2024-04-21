d11442250b110d345df528e9f010fa0090f6e39b
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2024-04-21 23:19:35 +0700

    Update NotFound.js

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import '../Css/NotFound.css'
+import { Link } from 'react-router-dom'
 function NotFound() {
     return (
-        <div>
-            <h2 style={{color: "red"}}>404 NotFound</h2>
-        </div>
+        // <div style={{minHeight:"55vh"}}>
+        //     <h2 style={{color: "red", fontSize: "4rem", paddingTop:"12%"}}>404 NotFound</h2>
+        // </div>
+        <>
+            <h1>404 Error Page</h1>
+            <p class="zoom-area"><b>CSS</b> animations to make a cool 404 page. </p>
+            <section class="error-container">
+                <span class="four"><span class="screen-reader-text">4</span></span>
+                <span class="zero"><span class="screen-reader-text">0</span></span>
+                <span class="four"><span class="screen-reader-text">4</span></span>
+            </section>
+            <div class="link-container">
+                <Link className='more-link' to={'/home'}>Tới trang chủ</Link>
+            </div>
+        </>
     )
 }
 
