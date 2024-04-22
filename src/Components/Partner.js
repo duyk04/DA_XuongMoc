9b4df040e9f1ca09fb8aa9e3a9ac9812a0b2a463
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2024-04-22 08:44:54 +0700

    Update Partner.js

--- src/Components/Partner.js
+++ src/Components/Partner.js
@@ -17,9 +17,9 @@ function Partner() {
 					<div className='box-container content' >
 						<div className='row my-5 '>
 							<div className='col-md-4 p-0'>
-								<img src="../images/AnhCatTC/doi-tac/marvella.png"
-                                                    className="w-100"
-                                                    alt='marvella'/>
+								<img src="../images/AnhCatTC/doi-tac/vinpearl.png"
+									className="w-100"
+									alt='marvella' />
 							</div>
 							<div className='col-md-1'></div>
 							<div className='col-md-7 p-0'>
@@ -27,12 +27,57 @@ function Partner() {
 								<p>Vinpearl là thương hiệu dịch vụ du lịch nghỉ dưỡng – giải trí lớn nhất Việt Nam. Vinpearl sở hữu chuỗi khách sạn, resort và trung tâm hội nghị đẳng cấp 5 sao, các khu vui chơi giải trí quốc tế tọa lạc tại những danh thắng du lịch nổi tiếng nhất của Việt Nam.</p>
 							</div>
 						</div>
+						<div className='row my-5 '>
+							<div className='col-md-4 p-0'>
+								<img src="../images/AnhCatTC/doi-tac/muong-thanh.png"
+									className="w-100"
+									alt='marvella' />
+							</div>
+							<div className='col-md-1'></div>
+							<div className='col-md-7 p-0'>
+								<h4>TẬP ĐOÀN KHÁCH SẠN MƯỜNG THANH</h4>
+								<p>Tại Mường Thanh, chúng tôi mời bạn cùng khởi hành chuyến đi tìm về không gian thanh thản chứa đựng những nét văn hóa mang đậm tinh thần bản sắc Việt, nơi con người gắn kết và thân ái gửi trao nhau tình cảm chân thành. </p>
+							</div>
+						</div>
+						<div className='row my-5 '>
+							<div className='col-md-4 p-0'>
+								<img src="../images/AnhCatTC/doi-tac/sheraton.png"
+									className="w-100"
+									alt='marvella' />
+							</div>
+							<div className='col-md-1'></div>
+							<div className='col-md-7 p-0'>
+								<h4>sheraton hanoI HOTEL</h4>
+								<p>Situated on the shores of Hanoi's West Lake and surrounded by its many local attractions, Sheraton Hanoi Hotel is just a quick drive to the bustling downtown of Hanoi City. Explore the nearby Old Quarter, home to Hoan Kiem Lake, Hanoi Opera House and exceptional boutiques.</p>
+							</div>
+						</div>
+						<div className='row my-5 '>
+							<div className='col-md-4 p-0'>
+								<img src="../images/AnhCatTC/doi-tac/the-coffee-house.png"
+									className="w-100"
+									alt='marvella' />
+							</div>
+							<div className='col-md-1'></div>
+							<div className='col-md-7 p-0'>
+								<h4>THE COFFEE HOUSE</h4>
+								<p>Tại The Coffee House, chúng tôi luôn trân trọng những câu chuyện và đề cao giá trị Kết nối con người. Chúng tôi mong muốn The Coffee House sẽ trở thành “Nhà Cà Phê", nơi mọi người xích lại gần nhau và tìm thấy niềm vui, sự sẻ chia thân tình bên những tách cà phê đượm hương, chất lượng.</p>
+							</div>
+						</div>
+						<div className='row my-5 '>
+							<div className='col-md-4 p-0'>
+								<img src="../images/AnhCatTC/doi-tac/marvella.png"
+									className="w-100"
+									alt='marvella' />
+							</div>
+							<div className='col-md-1'></div>
+							<div className='col-md-7 p-0'>
+								<h4>Marvella Hotel Nha Trang</h4>
+								<p>Marvella một thương hiệu khách sạn 4 sao mới xây dựng theo phong cách tân cổ điển, sang trọng.</p>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</>
-
 	)
-}
-
-export default Partner
+} export default Partner
