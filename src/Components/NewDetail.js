c43f04dc441aa62deff30ce6d98de8e538fc6b87
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2024-04-25 23:17:40 +0700

    Create NewDetail.js

---
+++ src/Components/NewDetail.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../Css/NewDetail.css'
+function NewDetail() {
+    return (
+        <>
+            <div className='NewDetail d-flex flex-column align-items-center'>
+                <div className='box-container my-5'>
+                    <div className='row m-5'>
+                        <div className='col-md-8 pe-5'>
+                            <p className='title'>
+                                NGẤT NGÂY VỚI TOP 10 MẪU NỘI THẤT CHUNG CƯ 1 PHÒNG NGỦ ĐẸP NHẤT
+                            </p>
+                            <p>
+                                Những căn hộ chung cư mini, có diện tích nhỏ ngày càng trở nên nên ưa chuộng hơn trong cuộc sống hiện đại với những ai có khoản kinh phí "hạn hẹp". Thiết kế nội thất chung cư 1 phòng ngủ chính là giải pháp tối ưu nhất để mang lại một không gian sống hoàn hảo và hiện đại.
+                                <br />
+                                Vậy hãy cùng Nội thất Mạnh Hệ xem qua bài viết dưới đây để tham khảo thêm nhiều ý tưởng thiết kế nội thất chung cư 1 phòng ngủ thật ấm cúng và tiện nghi nhé!
+                            </p>
+                            <img src='../images/AnhCatTC/New/new_4.png' alt='img' className='w-100' />
+                        </div>
+                        <div className='col-md-4'>
+                            <h4>Bài viết mới</h4>
+                            <div className='row mb-4'>
+                                <div className='col-4'>
+                                    <img loading='lazy' src='../images/AnhCatTC/Products/phong_khach/banuongnuoc_1.png' alt='img' className=' w-100' />
+                                </div>
+                                <div className='col-8'>
+                                    <p>
+                                        25+ MẪU GIƯỜNG NGỦ HỘC KÉO THÔNG MINH CHO CĂN PHÒNG NHỎ CỦA BẠN
+                                    </p>
+                                </div>
+                            </div>
+                            <div className='row mb-4'>
+                                <div className='col-4'>
+                                    <img loading='lazy' src='../images/AnhCatTC/Products/phong_khach/banuongnuoc_1.png' alt='img' className=' w-100' />
+                                </div>
+                                <div className='col-8'>
+                                    <p>
+                                        25+ MẪU GIƯỜNG NGỦ HỘC KÉO THÔNG MINH CHO CĂN PHÒNG NHỎ CỦA BẠN
+                                    </p>
+                                </div>
+                            </div>
+                            <div className='row mb-4'>
+                                <div className='col-4'>
+                                    <img loading='lazy' src='../images/AnhCatTC/Products/phong_khach/banuongnuoc_1.png' alt='img' className=' w-100' />
+                                </div>
+                                <div className='col-8'>
+                                    <p>
+                                        25+ MẪU GIƯỜNG NGỦ HỘC KÉO THÔNG MINH CHO CĂN PHÒNG NHỎ CỦA BẠN
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default NewDetail
