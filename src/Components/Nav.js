20b99ef5ad56a839918eefe0ba3155789698d1cd
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2024-06-10 21:43:03 +0700

    Update Nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,7 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import '../Css/Nav.css'
 function Nav() {
+    const location = useLocation();
+    const [activePath, setActivePath] = useState(location.pathname);
+
+    useEffect(() => {
+        setActivePath(location.pathname);
+    }, [location.pathname]);
+
+
     return (
         <div>
             <nav className='navbar w-100  navbar-light bg-light'>
@@ -12,25 +20,25 @@ function Nav() {
                         </Link>
                     </div>
                     <div className='float-end d-flex align-items-center' style={{ height: 96 }}>
-                        <div className='navbar-item '>
+                        <div className='navbar-item'>
                             <ul className='d-flex mb-0 h-100'>
-                                <li className="nav-item mx-4">
-                                    <Link to={"/Home"}>Trang chủ</Link>
+                                <li className={`nav-item mx-4 ${activePath === '/Home' ? 'nav-active' : ''}`}>
+                                    <Link to="/Home" >Trang chủ</Link>
                                 </li>
-                                <li className="nav-item mx-4">
-                                    <Link to={"/Introduce"}>Giới thiệu</Link>
+                                <li className={`nav-item mx-4 ${activePath === '/Introduce' ? 'nav-active' : ''}`}>
+                                    <Link to="/Introduce" >Giới thiệu</Link>
                                 </li>
-                                <li className="nav-item mx-4">
-                                    <Link to={"/Products"} state={{showAllProducts: true}}>Sản phẩm</Link>
+                                <li className={`nav-item mx-4 ${activePath === '/Products' ? 'nav-active' : ''}`}>
+                                    <Link to="/Products" state={{ showAllProducts: true }} >Sản phẩm</Link>
                                 </li>
-                                <li className="nav-item mx-4">
-                                    <Link to={"/New"}>Tin tức</Link>
+                                <li className={`nav-item mx-4 ${activePath === '/New' ? 'nav-active' : ''}`}>
+                                    <Link to="/New" >Tin tức</Link>
                                 </li>
-                                <li className="nav-item mx-4">
-                                    <Link to={"/Partner"}>Đối tác</Link>
+                                <li className={`nav-item mx-4 ${activePath === '/Partner' ? 'nav-active' : ''}`}>
+                                    <Link to="/Partner" >Đối tác</Link>
                                 </li>
-                                <li className="nav-item mx-4">
-                                    <Link to={"/Contact"}>Liên hệ</Link>
+                                <li className={`nav-item mx-4 ${activePath === '/Contact' ? 'nav-active' : ''}`}>
+                                    <Link to="/Contact" >Liên hệ</Link>
                                 </li>
                             </ul>
                         </div>
